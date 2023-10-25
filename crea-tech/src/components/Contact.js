2443567c3eef58c3fa8570452180c16a140be3ef
Rcohl <corentin.orczyk@outlook.fr>
2023-10-25 13:23:57 +0200

    modifications du formulaire + correction Header

--- crea-tech/src/components/Contact.js
+++ crea-tech/src/components/Contact.js
@@ -20,46 +20,86 @@ const Contact = () => {
       </div>
       <div className="content">
         <div className="contact-info">
-          <Info /> 
+          <Info />
         </div>
 
-        <form className='contact-form'>
-          <input
-            className="form-name"
-            id="name"
-            name="name"
-            placeholder="Nom et Prénom"
-            type="text"
-            required
-          />
-          <input
-            className="form-eamil"
-            id="email"
-            name="email"
-            defaultValue=""
-            render={({ field }) => <input {...field} />}
-            placeholder="Email"
-            type="email"
-            required
-          />
-          <input
-            className="form-sujet"
-            id="sujet"
-            name="sujet"
-            placeholder='Sujet'
-            type="text"
-            required
-          />
+        <form className="contact-form">
+          <label>
+            Nom
+            <input
+              className="form-name"
+              id="name"
+              name="name"
+              placeholder="Nom"
+              type="text"
+              required
+            />
+          </label>
 
+          <label>
+            Prénom
+            <input
+              className="form-firstname"
+              id="firstname"
+              name="firstname"
+              placeholder="Prénom"
+              type="text"
+              required
+            />
+          </label>
 
+          <label>
+            E-mail
+            <input
+              className="form-eamil"
+              id="email"
+              name="email"
+              defaultValue=""
+              render={({ field }) => <input {...field} />}
+              placeholder="Email"
+              type="email"
+              required
+            />
+          </label>
+          <label>
+            Sujet
+            <select
+              className="form-sujet"
+              id="select"
+              name="select"
+              placeholder="Sujet"
+              type="select"
+              required
+            >
+              <option value={""} hidden>
+                Choissir une option
+              </option>
+              <option value={"wordpress"}>Wordpress</option>
+              <option value={"formation"}>Formation</option>
+              <option value={"website"}>Création de site</option>
+            </select>
+          </label>
 
-          < textarea className='form-message'
-            id="message"
-            name="message"
-            placeholder='Message'
-            rows='3'
-          />
-          <button className='btn' type='submit'><a href="#"><span>Envoyer la demande </span></a></button>
+          <label>
+            Sélectionner un fichier
+            <input className="form-file" id="file" name="file" type="file" />
+          </label>
+
+          <label>
+            Un commentaire ? Une précision ? Dites-nous tout
+            <textarea
+              className="form-message"
+              id="message"
+              name="message"
+              placeholder="Message"
+              rows="5"
+            />
+          </label>
+          <button className="btn" type="submit">
+            <a href="#">
+              <span>Envoyer la demande </span>
+            </a>
+          </button>
         </form>
       </div>
     </section>
